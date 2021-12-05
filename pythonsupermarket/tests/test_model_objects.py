a32fc7a6d1cfc4806841ba0854a4bc81d7345f5e
Triin Tamra <triin.tamra@aggreko.com>
2021-12-05 22:35:50 +0100

    ISSUE-06: added test class TestDiscountIntegration.

--- pythonsupermarket/tests/test_model_objects.py
+++ pythonsupermarket/tests/test_model_objects.py
@@ -123,3 +123,29 @@ class TestOfferIntegration(unittest.TestCase):
     @pytest.mark.skip(reason="unsure at the moment what arguments exactly, will check later.")
     def test_asserts_argument_correctly(self):
         pass
+
+
+@pytest.mark.skip(reason="not a higher priority.")
+class TestDiscount(unittest.TestCase):
+    pass
+
+
+class TestDiscountIntegration(unittest.TestCase):
+    def setUp(self):
+        self.product_dict = set_up_product_dict()
+        self.test_class = mdl_objcts.Discount(product=mdl_objcts.Product(**cp.deepcopy(self.product_dict)),
+                                              description="It's a great discount!",
+                                              discount_amount="TODO, what exactly to put here?")
+
+    def test_asserts_product_correctly(self):
+        self.assertEqual(self.test_class.product.name, self.product_dict['name'])
+        self.assertEqual(self.test_class.product.unit, self.product_dict['unit'])
+
+
+    @pytest.mark.skip(reason="Unsure in what format the description should be (for now)")
+    def test_asserts_description_correctly(self):
+        pass
+
+    @pytest.mark.skip(reason="Unsure in what format the discount amount should be (for now)")
+    def test_asserts_discount_amount_correctly(self):
+        pass
