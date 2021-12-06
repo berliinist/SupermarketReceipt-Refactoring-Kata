08c173ead8fe1da398d61beef865d78c9d4c4951
Triin Tamra <triin.tamra@aggreko.com>
2021-12-06 08:26:23 +0100

    ISSUE-08: further refactoring of test_shopping_cart.

--- pythonsupermarket/tests/test_shopping_cart.py
+++ pythonsupermarket/tests/test_shopping_cart.py
@@ -111,105 +111,72 @@ class TestShoppingCartIntegration(unittest.TestCase):
         self.assertEqual(len(self.handle_kw_args['receipt'].discounts), len(products))
 
 
-class TestShoppingCartHandleOffersThreeForTwo(unittest.TestCase):
-    def setUp(self):
-        self.toothbrush = mdl_objcts.Product('toothbrush', mdl_objcts.ProductUnit.EACH)
-        self.offers = {self.toothbrush: mdl_objcts.Offer(mdl_objcts.SpecialOfferType.THREE_FOR_TWO, self.toothbrush, '')}
-        self.handle_kw_args = {'receipt': Receipt(), 'offers': self.offers, 'catalog': FakeCatalog()}
-        self.toothbrush_unit_price = 0.99
-        self.handle_kw_args['catalog'].add_product(self.toothbrush, self.toothbrush_unit_price)
-
-        self.shoppingcart = ShoppingCart()
-
-    def test_no_discount_for_too_little_items_in_cart(self):
-        self.shoppingcart.add_item_quantity(self.toothbrush, 2)
-        self.shoppingcart.handle_offers(**self.handle_kw_args)
-        self.assertListEqual(self.handle_kw_args['receipt'].discounts, [])
-
-    def test_discount_list_for_pay_two_for_three_items_in_cart(self):
-        self.shoppingcart.add_item_quantity(self.toothbrush, 3)
-        self.shoppingcart.handle_offers(**self.handle_kw_args)
-
-        self.assertEqual(len(self.handle_kw_args['receipt'].discounts), 1)
-
-    def test_discount_correct_amount_to_pay_two_for_three_items_in_cart(self):
-        self.shoppingcart.add_item_quantity(self.toothbrush, 3)
-        self.shoppingcart.handle_offers(**self.handle_kw_args)
-
-        self.assertAlmostEqual(self.handle_kw_args['receipt'].discounts[0].discount_amount,
-                               -self.toothbrush_unit_price, places=3)
-
-
-class TestShoppingCartHandleOffersOfPercentDiscount(unittest.TestCase):
-
-    def _set_up_kw_args_and_add_product(self, p_name, unit_price, percentage, type):
-        self.product = mdl_objcts.Product(p_name, type)
-        self.offers = {self.product: mdl_objcts.Offer(mdl_objcts.SpecialOfferType.TEN_PERCENT_DISCOUNT,
-                                                      self.product,
-                                                      percentage)}
+class SharedHandleOffersSetups:
+    def _set_up_kw_args_and_add_product(self, name, unit_price, offer_arg, quantity, p_type, offer_type):
+        self.product = mdl_objcts.Product(name, p_type)
+        self.offers = {self.product: mdl_objcts.Offer(offer_type, self.product, offer_arg)}
         self.handle_kw_args = {'receipt': Receipt(), 'offers': self.offers, 'catalog': FakeCatalog()}
         self.unit_price = unit_price
         self.handle_kw_args['catalog'].add_product(self.product, self.unit_price)
 
         self.shoppingcart = ShoppingCart()
-        self.shoppingcart.add_item_quantity(self.product, 1)
+        self.shoppingcart.add_item_quantity(self.product, quantity)
         self.shoppingcart.handle_offers(**self.handle_kw_args)
 
+
+class TestShoppingCartHandleOffersThreeForTwo(unittest.TestCase, SharedHandleOffersSetups):
+    def test_no_discount_for_too_little_items_in_cart(self):
+        self._set_up_kw_args_and_add_product('toothbrush', 0.99, '', 2, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.THREE_FOR_TWO)
+        self.assertListEqual(self.handle_kw_args['receipt'].discounts, [])
+
+    def test_discount_list_for_pay_two_for_three_items_in_cart(self):
+        self._set_up_kw_args_and_add_product('toothbrush', 0.99, '', 3, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.THREE_FOR_TWO)
+        self.assertEqual(len(self.handle_kw_args['receipt'].discounts), 1)
+
+    def test_discount_correct_amount_to_pay_two_for_three_items_in_cart(self):
+        self._set_up_kw_args_and_add_product('toothbrush', 0.99, '', 3, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.THREE_FOR_TWO)
+        self.shoppingcart.handle_offers(**self.handle_kw_args)
+
+        self.assertAlmostEqual(self.handle_kw_args['receipt'].discounts[0].discount_amount,
+                               -self.unit_price, places=3)
+
+
+class TestShoppingCartHandleOffersOfPercentDiscount(unittest.TestCase, SharedHandleOffersSetups):
     def test_discount_list_length_of_one_for_specific_item_in_cart(self):
-        self._set_up_kw_args_and_add_product('rice', 2.49, 10, mdl_objcts.ProductUnit.EACH)
+        self._set_up_kw_args_and_add_product('rice', 2.49, 10, 1, mdl_objcts.ProductUnit.EACH, mdl_objcts.SpecialOfferType.TEN_PERCENT_DISCOUNT)
         self.assertEqual(len(self.handle_kw_args['receipt'].discounts), 1)
 
     @parameterized.expand([('rice', 2.49, 10, mdl_objcts.ProductUnit.EACH), ('apple', 1.99, 20, mdl_objcts.ProductUnit.KILO)])
-    def test_discount_correct_percent_for_specific_item_in_cart(self, product, unit_price, percent, type):
-        self._set_up_kw_args_and_add_product(product, unit_price, percent, type)
+    def test_discount_correct_percent_for_specific_item_in_cart(self, product, unit_price, percent, p_type):
+        self._set_up_kw_args_and_add_product(product, unit_price, percent, 1, p_type, mdl_objcts.SpecialOfferType.TEN_PERCENT_DISCOUNT)
         expected = -percent / 100 * self.unit_price
         self.assertAlmostEqual(self.handle_kw_args['receipt'].discounts[0].discount_amount, expected, places=3)
 
 
-class TestShoppingCartHandleOffersOfTwoForAmount(unittest.TestCase):
-
-    def _set_up_kw_args_and_add_product(self, unit_price, offer_price, quantity):
-        self.product = mdl_objcts.Product('cherrytomatoes', mdl_objcts.ProductUnit.EACH)
-        self.offers = {self.product: mdl_objcts.Offer(mdl_objcts.SpecialOfferType.TWO_FOR_AMOUNT,
-                                                      self.product,
-                                                      offer_price)}
-        self.handle_kw_args = {'receipt': Receipt(), 'offers': self.offers, 'catalog': FakeCatalog()}
-        self.unit_price = unit_price
-        self.handle_kw_args['catalog'].add_product(self.product, self.unit_price)
-
-        self.shoppingcart = ShoppingCart()
-        self.shoppingcart.add_item_quantity(self.product, quantity)
-        self.shoppingcart.handle_offers(**self.handle_kw_args)
-
+class TestShoppingCartHandleOffersOfTwoForAmount(unittest.TestCase, SharedHandleOffersSetups):
     def test_discounts_for_two_quantities_item_with_offer_price_two_for_amount(self):
-        self._set_up_kw_args_and_add_product(0.69, 0.99, 2)
+        self._set_up_kw_args_and_add_product('cherrytomatoes', 0.69, 0.99, 2, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.TWO_FOR_AMOUNT)
         expected = -0.39  # TODO: do math to get this value
         self.assertAlmostEqual(self.handle_kw_args['receipt'].discounts[0].discount_amount, expected, places=3)
 
     def test_no_discounts_for_less_quantities_item_with_offer_price_two_for_amount(self):
-        self._set_up_kw_args_and_add_product(0.69, 0.99, 1)
+        self._set_up_kw_args_and_add_product('cherrytomatoes', 0.69, 0.99, 1, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.TWO_FOR_AMOUNT)
         self.assertListEqual(self.handle_kw_args['receipt'].discounts, [])
 
 
-class TestShoppingCartHandleOffersOfFiveForAmount(unittest.TestCase):
-    def _set_up_kw_args_and_add_product(self, unit_price, offer_price, quantity):
-        self.product = mdl_objcts.Product('toothpaste', mdl_objcts.ProductUnit.EACH)
-        self.offers = {self.product: mdl_objcts.Offer(mdl_objcts.SpecialOfferType.FIVE_FOR_AMOUNT,
-                                                      self.product,
-                                                      offer_price)}
-        self.handle_kw_args = {'receipt': Receipt(), 'offers': self.offers, 'catalog': FakeCatalog()}
-        self.unit_price = unit_price
-        self.handle_kw_args['catalog'].add_product(self.product, self.unit_price)
-
-        self.shoppingcart = ShoppingCart()
-        self.shoppingcart.add_item_quantity(self.product, quantity)
-        self.shoppingcart.handle_offers(**self.handle_kw_args)
-
+class TestShoppingCartHandleOffersOfFiveForAmount(unittest.TestCase, SharedHandleOffersSetups):
     def test_discounts_for_five_quantities_item_with_offer_price_five_for_amount(self):
-        self._set_up_kw_args_and_add_product(1.79, 7.49, 5)
+        self._set_up_kw_args_and_add_product('toothpaste', 1.79, 7.49, 5, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.FIVE_FOR_AMOUNT)
         expected = -1.46  # TODO: do math to get this value
         self.assertAlmostEqual(self.handle_kw_args['receipt'].discounts[0].discount_amount, expected, places=3)
 
     def test_no_discounts_for_less_quantities_item_with_offer_price_five_for_amount(self):
-        self._set_up_kw_args_and_add_product(1.79, 7.49, 4)
+        self._set_up_kw_args_and_add_product('toothpaste', 1.79, 7.49, 4, mdl_objcts.ProductUnit.EACH,
+                                             mdl_objcts.SpecialOfferType.FIVE_FOR_AMOUNT)
         self.assertListEqual(self.handle_kw_args['receipt'].discounts, [])
