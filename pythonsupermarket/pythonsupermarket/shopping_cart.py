4075a476d916c9dceb8dfbb12183e20b446b6d93
Triin Tamra <triin.tamra@aggreko.com>
2021-12-06 06:12:34 +0100

    ISSUE-08: removed add_item from shopping cart and did some refactorings.

--- pythonsupermarket/pythonsupermarket/shopping_cart.py
+++ pythonsupermarket/pythonsupermarket/shopping_cart.py
@@ -13,9 +13,6 @@ class ShoppingCart:
     def items(self):
         return self._items
 
-    def add_item(self, product):  # TODO: this is not used anywhere, what for? Remove it?
-        self.add_item_quantity(product, 1.0)
-
     @property
     def product_quantities(self):
         return self._product_quantities
@@ -23,7 +20,7 @@ class ShoppingCart:
     def add_item_quantity(self, product, quantity):
         self._items.append(ProductQuantity(product, quantity))
         if product in self._product_quantities.keys():
-            self._product_quantities[product] = self._product_quantities[product] + quantity
+            self._product_quantities[product] += quantity
         else:
             self._product_quantities[product] = quantity
 
@@ -32,37 +29,41 @@ class ShoppingCart:
             quantity = self._product_quantities[p]
             if p in offers.keys():
                 offer = offers[p]
-                unit_price = catalog.unit_price(p)
-                quantity_as_int = int(quantity)
-                discount = None
-                x = 1
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
-                    x = 3
-
-                elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
-                    x = 2
-                    if quantity_as_int >= 2:
-                        total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
-                        discount_n = unit_price * quantity - total
-                        discount = Discount(p, "2 for " + str(offer.argument), -discount_n)
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
-                    x = 5
-
-                number_of_x = math.floor(quantity_as_int / x)
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
-                    discount_amount = quantity * unit_price - (
-                                (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-                    discount = Discount(p, "3 for 2", -discount_amount)
-
-                if offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:  # TODO: rename it to PERCENT_DISCOUNT
-                    discount = Discount(p, str(offer.argument) + "% off",
-                                        -quantity * unit_price * offer.argument / 100.0)
-
-                if offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
-                    discount_total = unit_price * quantity - (
-                                offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
-                    discount = Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
+                discount = self._get_discount(offer, catalog, quantity, p)
 
                 if discount:
                     receipt.add_discount(discount)
+
+    def _get_discount(self, offer, catalog, quantity, p):
+        quantity_as_int = int(quantity)
+        unit_price = catalog.unit_price(p)
+
+        def _get_x_denominator(offer_type):
+            if offer_type == SpecialOfferType.THREE_FOR_TWO:
+                return 3
+            elif offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
+                return 2
+            elif offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
+                return 5
+            else:
+                return 1
+        x = _get_x_denominator(offer.offer_type)
+        number_of_x = math.floor(quantity_as_int / x)
+
+        if offer.offer_type == SpecialOfferType.THREE_FOR_TWO and quantity_as_int > 2:
+            discount_amount = quantity * unit_price - (
+                    (number_of_x * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+            return Discount(p, "3 for 2", -discount_amount)
+        elif offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:  # TODO: rename it to PERCENT_DISCOUNT
+            return Discount(p, str(offer.argument) + "% off",
+                                -quantity * unit_price * offer.argument / 100.0)
+        elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT and quantity_as_int >= 2:
+            total = offer.argument * (quantity_as_int / x) + quantity_as_int % 2 * unit_price
+            discount_n = unit_price * quantity - total
+            return Discount(p, "2 for " + str(offer.argument), -discount_n)
+        elif offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT and quantity_as_int >= 5:
+            discount_total = unit_price * quantity - (
+                    offer.argument * number_of_x + quantity_as_int % 5 * unit_price)
+            return Discount(p, str(x) + " for " + str(offer.argument), -discount_total)
+        else:
+            return None
