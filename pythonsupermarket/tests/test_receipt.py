b6d618c8a5eab2e0d576818dee04d0396579c900
Triin Tamra <triin.tamra@aggreko.com>
2021-12-07 21:33:06 +0100

    ISSUE-12: more refactoring to test_receipt.

--- pythonsupermarket/tests/test_receipt.py
+++ pythonsupermarket/tests/test_receipt.py
@@ -58,12 +58,25 @@ class TestReceiptIntegration(unittest.TestCase):
                              'unit_price': round(random.random() * 100, 2),
                              'total_price': round(random.random() * 200, 2)} for i, _ in enumerate(range(nr_items))]
 
-    def _setup_list_kwargs_of_each_discount(self, nr_items):
-        self.prod_kwargs_list = [setup_product_kwargs() for _ in range(nr_items)]
-        self.kw_args_disc_list = [
+    def _setup_and_add_items_to_receipt(self, nr_items):
+        self._setup_list_kwargs_of_each_item(nr_items)
+        for i in range(nr_items):
+            self.receipt.add_item(**self.kwargs_list[i])
+
+    def _setup_list_kwargs_of_each_discount(self, nr_discounts):
+        self.prod_kwargs_list = [setup_product_kwargs() for _ in range(nr_discounts)]
+        self.disc_kwargs_list = [
             {'product': mdl_objcts.ProductInfo(**cp.deepcopy(self.prod_kwargs_list[i])),
              'description': f'Some discount number {i+1}',
-             'discount_amount': round(random.random() * -10, 2)} for i, _ in enumerate(range(nr_items))]
+             'discount_amount': round(random.random() * -10, 2)} for i, _ in enumerate(range(nr_discounts))]
+
+    def _setup_and_add_discounts_to_receipt(self, nr_discounts):
+        self._setup_list_kwargs_of_each_discount(nr_discounts)
+
+        discounts = [mdl_objcts.Discount(**self.disc_kwargs_list[i]) for i in range(nr_discounts)]
+        for i in range(nr_discounts):
+            self.receipt.add_discount(discounts[i])
+        return discounts
 
     def test_initial_receipt_items_returns_empty(self):
         self.assertListEqual(self.receipt.items, [])
@@ -81,56 +94,42 @@ class TestReceiptIntegration(unittest.TestCase):
 
     @parameterized.expand([(1,), (10,)])
     def test_assert_number_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertEqual(len(self.receipt.items), nr_items)
 
     @parameterized.expand([(1,), (8,)])
     def test_assert_correct_order_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].item for i in range(nr_items)],
                              [self.kwargs_list[i]['item'] for i in range(nr_items)])
 
     @parameterized.expand([(1,), (8,)])
     def test_assert_names_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].item.name for i in range(nr_items)],
                              [self.prod_kwargs_list[i]['name'] for i in range(nr_items)])
 
     @parameterized.expand([(1,), (7,)])
     def test_assert_units_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].item.unit for i in range(nr_items)],
                              [self.prod_kwargs_list[i]['unit'] for i in range(nr_items)])
 
     @parameterized.expand([(1,), (6,)])
     def test_assert_quantities_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].quantity for i in range(nr_items)],
                              [self.kwargs_list[i]['quantity'] for i in range(nr_items)])
 
     @parameterized.expand([(1,), (6,)])
     def test_assert_unit_prices_of_items(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].unit_price for i in range(nr_items)],
                              [self.kwargs_list[i]['unit_price'] for i in range(nr_items)])
 
     @parameterized.expand([(1,), (5,)])
     def test_assert_total_price_of_each_item(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         self.assertListEqual([self.receipt.items[i].total_price for i in range(nr_items)],
                              [self.kwargs_list[i]['total_price'] for i in range(nr_items)])
 
@@ -143,68 +142,51 @@ class TestReceiptIntegration(unittest.TestCase):
 
     @parameterized.expand([(1,), (4,)])
     def test_assert_correct_number_of_discounts_after_added(self, nr_discounts):
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        for i in range(nr_discounts):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
+        _ = self._setup_and_add_discounts_to_receipt(nr_discounts)
         self.assertEqual(len(self.receipt.discounts), nr_discounts)
 
     @parameterized.expand([(1,), (4,)])
     def test_assert_correct_order_of_discounts(self, nr_discounts):
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        discounts = [mdl_objcts.Discount(**self.kw_args_disc_list[i]) for i in range(nr_discounts)]
-        for i in range(nr_discounts):
-            self.receipt.add_discount(discounts[i])
+        discounts = self._setup_and_add_discounts_to_receipt(nr_discounts)
         self.assertListEqual(self.receipt.discounts, discounts)
 
     @parameterized.expand([(1,), (3,)])
-    def test_assert_names_of_discounts(self, nr_items):
-        self._setup_list_kwargs_of_each_discount(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
-        self.assertListEqual([self.receipt.discounts[i].product.name for i in range(nr_items)],
-                             [self.prod_kwargs_list[i]['name'] for i in range(nr_items)])
+    def test_assert_names_of_discounts(self, nr_discounts):
+        _ = self._setup_and_add_discounts_to_receipt(nr_discounts)
+        self.assertListEqual([self.receipt.discounts[i].product.name for i in range(nr_discounts)],
+                             [self.prod_kwargs_list[i]['name'] for i in range(nr_discounts)])
 
     @parameterized.expand([(1,), (4,)])
     def test_assert_descriptions_of_discounts(self, nr_discounts):
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        for i in range(nr_discounts):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
+        _ = self._setup_and_add_discounts_to_receipt(nr_discounts)
         self.assertListEqual([self.receipt.discounts[i].description for i in range(nr_discounts)],
-                             [self.kw_args_disc_list[i]['description'] for i in range(nr_discounts)])
+                             [self.disc_kwargs_list[i]['description'] for i in range(nr_discounts)])
 
     @parameterized.expand([(1,), (2, )])
     def test_assert_discount_amounts(self, nr_discounts):
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        for i in range(nr_discounts):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
+        _ = discounts = self._setup_and_add_discounts_to_receipt(nr_discounts)
         self.assertListEqual([self.receipt.discounts[i].discount_amount for i in range(nr_discounts)],
-                             [self.kw_args_disc_list[i]['discount_amount'] for i in range(nr_discounts)])
+                             [self.disc_kwargs_list[i]['discount_amount'] for i in range(nr_discounts)])
 
     @parameterized.expand([(1,), (5,)])
     def test_assert_receipt_total_price_if_items_only(self, nr_items):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
+        self._setup_and_add_items_to_receipt(nr_items)
         expected = round(sum([self.kwargs_list[i]['total_price'] for i in range(nr_items)]), 2)
         self.assertAlmostEqual(self.receipt.total_price(), expected, places=2)
 
     @parameterized.expand([(1,), (7,)])
     def test_assert_receipt_total_price_if_discounts_only(self, nr_discounts):
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        for i in range(nr_discounts):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
-        expected = round(sum([self.kw_args_disc_list[i]['discount_amount'] for i in range(nr_discounts)]), 2)
+        _ = discounts = self._setup_and_add_discounts_to_receipt(nr_discounts)
+        expected = round(sum([self.disc_kwargs_list[i]['discount_amount'] for i in range(nr_discounts)]), 2)
         self.assertAlmostEqual(self.receipt.total_price(), expected, places=2)
 
     @parameterized.expand([(1, 1), (1, 7), (7, 1)])
     def test_assert_receipt_total_price_of_items_and_discounts(self, nr_items, nr_discounts):
-        self._setup_list_kwargs_of_each_item(nr_items)
-        self._setup_list_kwargs_of_each_discount(nr_discounts)
-        for i in range(nr_items):
-            self.receipt.add_item(**self.kwargs_list[i])
-        for i in range(nr_discounts):
-            self.receipt.add_discount(mdl_objcts.Discount(**self.kw_args_disc_list[i]))
+        self._setup_and_add_items_to_receipt(nr_items)
+        _ = self._setup_and_add_discounts_to_receipt(nr_discounts)
+
         total_price = round(sum([self.kwargs_list[i]['total_price'] for i in range(nr_items)]), 2)
-        total_discounts = round(sum([self.kw_args_disc_list[i]['discount_amount'] for i in range(nr_discounts)]), 2)
+        total_discounts = round(sum([self.disc_kwargs_list[i]['discount_amount'] for i in range(nr_discounts)]), 2)
+
         expected_total_price =  total_price + total_discounts
         self.assertAlmostEqual(self.receipt.total_price(), expected_total_price, places=2)
