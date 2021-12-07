f8f8dd495ee6f5f6a1c496978be6a379776ea429
Triin Tamra <triin.tamra@aggreko.com>
2021-12-07 16:39:28 +0100

    ISSUE_11: solidified catalog and offers in Teller as properties.

--- pythonsupermarket/pythonsupermarket/teller.py
+++ pythonsupermarket/pythonsupermarket/teller.py
@@ -5,17 +5,25 @@ from pythonsupermarket.receipt import Receipt
 class Teller:
 
     def __init__(self, catalog):
-        self.catalog = catalog
-        self.offers = {}
+        self._catalog = catalog
+        self._offers = {}
+
+    @property
+    def catalog(self):
+        return self._catalog
+
+    @property
+    def offers(self):
+        return self._offers
 
     def add_special_offer(self, offer_type, product, argument):
-        self.offers[product] = Offer(offer_type, argument)
+        self._offers[product] = Offer(offer_type, argument)
 
     def checks_out_articles_from(self, the_cart):
         receipt = Receipt()
         for item, quantity in the_cart.items.items():
-            unit_price = self.catalog.get_product(item).price_per_unit
+            unit_price = self._catalog.get_product(item).price_per_unit
             receipt.add_product(item, quantity, unit_price, quantity * unit_price)
-        the_cart.handle_offers(receipt, self.offers, self.catalog)
+        the_cart.handle_offers(receipt, self._offers, self._catalog)
 
         return receipt
--- pythonsupermarket/tests/test_teller.py
+++ pythonsupermarket/tests/test_teller.py
@@ -31,44 +31,62 @@ class TestTellerIntegration(unittest.TestCase):
         teller = Teller(self.catalog)
         self.assertDictEqual(teller.offers, {})
 
+    def test_refuses_setting_catalog_property(self):
+        teller = Teller(self.catalog)
+        with self.assertRaises(AttributeError):
+            teller.catalog = {}
+
+    def test_refuses_setting_offers_property(self):
+        teller = Teller(self.catalog)
+        with self.assertRaises(AttributeError):
+            teller.offers = {}
+
     def _create_products_and_call_add_product(self, nr_products):
         self.product_dicts_list = [set_up_product_dict() for _ in range(nr_products)]
         self.products = [ProductInfo(**cp.deepcopy(self.product_dicts_list[i])) for i in range(nr_products)]
         for i in range(nr_products):
             self.catalog.add_product(self.products[i])
 
-    def _create_offers_and_add_them_to_teller(self, nr_offers):
+    def _create_offers_to_add_to_teller_and_return_expected_kwargs(self, nr_offers):
+        expected = {}
         for i in range(nr_offers):
-            self.teller.add_special_offer(offer_type=SpecialOfferType.TEN_PERCENT_DISCOUNT,
-                                          product=self.products[i],
-                                          argument=round(random.random() * 10, 2))
+            kwargs = {'offer_type': random.choice([SpecialOfferType.TEN_PERCENT_DISCOUNT,
+                                                   SpecialOfferType.THREE_FOR_TWO,
+                                                   SpecialOfferType.TWO_FOR_AMOUNT]),
+                      'argument': round(random.random() * 10, 2)}
+            self.teller.add_special_offer(**kwargs, product=self.products[i])
+            expected.update({self.products[i]: cp.deepcopy(kwargs)})
+        return expected
 
     @parameterized.expand([(1,), (3,)])
     def test_add_special_offer_adds_each_offer_correctly(self, nr_offers):
         self._create_products_and_call_add_product(nr_offers)
         self.teller = Teller(self.catalog)
-        self._create_offers_and_add_them_to_teller(nr_offers)
+        expected = self._create_offers_to_add_to_teller_and_return_expected_kwargs(nr_offers)
 
-        self.assertEqual(len(self.teller.offers), nr_offers)
         for product in self.products:
             self.assertIsInstance(self.teller.offers[product], Offer)
+            self.assertEqual(self.teller.offers[product].offer_type, expected[product]['offer_type'])
+            self.assertEqual(self.teller.offers[product].argument, expected[product]['argument'])
+        self.assertEqual(len(self.teller.offers), nr_offers)
 
     @parameterized.expand([(1,), (3,)])
-    def test_assert_checks_out_articles_and_returns_receipt_successfully(self, nr_unique_products):
-        self._create_products_and_call_add_product(nr_unique_products)
+    def test_checks_out_articles_and_returns_receipt_correctly(self, nr_products):
+        self._create_products_and_call_add_product(nr_products)
         quantity = []
-        for i in range(nr_unique_products):
+        for i in range(nr_products):
             quantity.append(round(random.random() * 10, 2))
             self.cart.add_item_quantity(item=cp.deepcopy(self.products[i]), quantity=quantity[-1])
         teller = Teller(self.catalog)
 
-        receipt_result = teller.checks_out_articles_from(self.cart)
-        expected = sum(quantity[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_unique_products))
-        self.assertAlmostEqual(receipt_result.total_price(), expected, places=2)
+        receipt = teller.checks_out_articles_from(self.cart)
+        expected = sum(quantity[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_products))
+        self.assertAlmostEqual(receipt.total_price(), expected, places=2)
 
     def test_returns_empty_receipt_if_no_items_in_cart(self):
-        nr_unique_products = 3
-        self._create_products_and_call_add_product(nr_unique_products)
+        self._create_products_and_call_add_product(nr_products=3)
         teller = Teller(self.catalog)
+
         receipt_result = teller.checks_out_articles_from(self.cart)
         self.assertEqual(receipt_result.total_price(), 0)
+        self.assertListEqual(receipt_result.items, [])
