5ebb9d505a04d484367d79ffa13b2cc564e78706
Triin Tamra <triin.tamra@aggreko.com>
2021-12-07 13:34:51 +0100

    ISSUE-11: changed unit approach in set_up_product_dict in test for better clarity.

--- pythonsupermarket/tests/shared_test_functions.py
+++ pythonsupermarket/tests/shared_test_functions.py
@@ -1,15 +1,21 @@
 from collections import namedtuple
+from enum import Enum
 import random
 import string
+
+
+class ProductUnitForTestOnly(Enum):
+    EACH = 1
+    KILO = 2
 
 
 PRODUCT_NAMEDTUPLE = namedtuple('product', ['name', 'unit', 'price_per_unit'])  # TODO: review if we still want to keep this namedtuple here?
 
 
-def set_up_product_dict():
-    return {'name': ''.join(random.choice(string.ascii_lowercase) for x in range(5)).capitalize(),
-            'unit': random.choice([1, 2]),
-            'price_per_unit': round(random.random() * 100, 2)}
+def set_up_product_dict():  # TODO: think of a better name. set_up_product_input_kwargs?
+    return {'name':             ''.join(random.choice(string.ascii_lowercase) for x in range(5)).capitalize(),
+            'unit':             random.choice([ProductUnitForTestOnly.EACH, ProductUnitForTestOnly.KILO]),
+            'price_per_unit':   round(random.random() * 100, 2)}
 
 
 class SharedUnitTests:
