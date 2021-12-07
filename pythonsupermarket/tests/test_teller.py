fc2a30dcf18253b51b5e1205ff4a83b55e56a4c6
Triin Tamra <triin.tamra@aggreko.com>
2021-12-07 22:03:12 +0100

    ISSUE_12: refactored test_teller.

--- pythonsupermarket/tests/test_teller.py
+++ pythonsupermarket/tests/test_teller.py
@@ -35,13 +35,13 @@ class TestTellerIntegration(unittest.TestCase):
         with self.assertRaises(AttributeError):
             teller.offers = {}
 
-    def _create_products_and_call_add_product(self, nr_products):
+    def _create_and_add_products_to_catalog(self, nr_products):
         self.product_dicts_list = [setup_product_kwargs() for _ in range(nr_products)]
         self.products = [ProductInfo(**cp.deepcopy(self.product_dicts_list[i])) for i in range(nr_products)]
         for i in range(nr_products):
             self.catalog.add_product(self.products[i])
 
-    def _create_offers_to_add_to_teller_and_return_expected_kwargs(self, nr_offers):
+    def _create_and_add_offers_and_return_expected_kwargs(self, nr_offers):
         expected = {}
         for i in range(nr_offers):
             kwargs = {'offer_type': random.choice([SpecialOfferType.PERCENT_DISCOUNT,
@@ -53,10 +53,10 @@ class TestTellerIntegration(unittest.TestCase):
         return expected
 
     @parameterized.expand([(1,), (3,)])
-    def test_add_special_offer_adds_each_offer_correctly(self, nr_offers):
-        self._create_products_and_call_add_product(nr_offers)
+    def test_assert_correct_order_and_contents_of_added_offers(self, nr_offers):
+        self._create_and_add_products_to_catalog(nr_offers)
         self.teller = Teller(self.catalog)
-        expected = self._create_offers_to_add_to_teller_and_return_expected_kwargs(nr_offers)
+        expected = self._create_and_add_offers_and_return_expected_kwargs(nr_offers)
 
         for product in self.products:
             self.assertIsInstance(self.teller.offers[product], Offer)
@@ -64,47 +64,51 @@ class TestTellerIntegration(unittest.TestCase):
             self.assertEqual(self.teller.offers[product].argument, expected[product]['argument'])
         self.assertEqual(len(self.teller.offers), nr_offers)
 
-    @parameterized.expand([(1,), (3,)])
-    def test_checks_out_articles_and_returns_receipt_correctly(self, nr_products):
-        self._create_products_and_call_add_product(nr_products)
-        quantity = []
+    def _create_and_add_items_to_the_cart_and_return_expected(self, nr_products):
+        self._create_and_add_products_to_catalog(nr_products)
+        quantities = []
         for i in range(nr_products):
-            quantity.append(round(random.random() * 10, 2))
-            self.cart.add_item_quantity(item=self.products[i], quantity=quantity[-1])
-        teller = Teller(self.catalog)
+            quantities.append(round(random.random() * 10, 2))
+            self.cart.add_item_quantity(item=self.products[i], quantity=quantities[-1])
+        expected = sum(quantities[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_products))
+        return expected, quantities
 
+    @parameterized.expand([(1,), (3,)])
+    def test_assert_correct_total_price_after_checkout_of_articles(self, nr_products):
+        expected, _ = self._create_and_add_items_to_the_cart_and_return_expected(nr_products)
+        teller = Teller(self.catalog)
         receipt = teller.checks_out_articles_from(self.cart)
-        expected = sum(quantity[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_products))
+
         self.assertAlmostEqual(receipt.total_price(), expected, places=2)
 
-    @parameterized.expand([(1,), (3,)])
-    def test_checks_out_articles_with_one_article_percent_discounted_and_returns_receipt_correctly(self, nr_products):
-        self._create_products_and_call_add_product(nr_products)
+    def _setup_item_to_discount_and_return_expected(self, quantities):
+        nr_products = len(self.products)
         kwargs = {'offer_type': SpecialOfferType.PERCENT_DISCOUNT, 'argument': round(random.random() * 10, 2)}
         d_i = random.randrange(0, nr_products)
 
-        quantity = []
-        for i in range(nr_products):
-            quantity.append(round(random.random() * 10, 2))
-            self.cart.add_item_quantity(item=self.products[i], quantity=quantity[-1])
 
+        expected = sum(quantities[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_products) if i != d_i)
+        expected += quantities[d_i] * self.product_dicts_list[d_i]['price_per_unit'] * (1 - kwargs['argument'] / 100.)
+        return expected, d_i, kwargs
+
+    @parameterized.expand([(1,), (3,)])
+    def test_checks_out_articles_with_one_article_percent_discounted_and_returns_receipt_correctly(self, nr_products):
+        _, quantities = self._create_and_add_items_to_the_cart_and_return_expected(nr_products)
         self.teller = Teller(self.catalog)
         receipt = self.teller.checks_out_articles_from(self.cart)
         total_price_before_discounts = receipt.total_price()
 
-        self.teller.add_special_offer(**kwargs, item=self.products[d_i])
+        expected, discount_index, kwargs = self._setup_item_to_discount_and_return_expected(quantities)
+        self.teller.add_special_offer(**kwargs, item=self.products[discount_index])
+
         receipt = self.teller.checks_out_articles_from(self.cart)
         total_price_after_discounts = receipt.total_price()
 
         self.assertLess(total_price_after_discounts, total_price_before_discounts)
-
-        expected = sum(quantity[i] * self.product_dicts_list[i]['price_per_unit'] for i in range(nr_products) if i != d_i)
-        expected += quantity[d_i] * self.product_dicts_list[d_i]['price_per_unit'] * (1 - kwargs['argument'] / 100.)
-
         self.assertAlmostEqual(total_price_after_discounts, expected, 2)
 
-    def test_returns_empty_receipt_if_no_items_in_cart(self):
-        self._create_products_and_call_add_product(nr_products=3)
+    def test_returns_empty_receipt_and_zero_total_price_if_no_items_in_cart(self):
+        self._create_and_add_products_to_catalog(nr_products=3)
         teller = Teller(self.catalog)
 
         receipt_result = teller.checks_out_articles_from(self.cart)
